53906403333962d13cc6b83fabc8a245d3f102e5
David Yuan <davidy804@gmail.com>
2019-12-08 16:15:39 -0500

    Cart works, quantity and remove button issues

--- src/app/appRoutes.ts
+++ src/app/appRoutes.ts
@@ -3,11 +3,13 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { HomeComponent } from './home/home.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { TermsComponent } from './terms/terms.component';
+import { CartComponent } from './cart/cart.component';
 
 export const appRoutes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'product',  component: ProductListComponent },
   { path: 'reviews/:id',  component: ReviewsComponent },
+  { path: 'cart', component: CartComponent },
   { path: 'terms', component: TermsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
